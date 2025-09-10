e2e90bc140e0fe62414410cb4b6c26ca2343a888
student-niranjan <155844421+student-niranjan@users.noreply.github.com>
2025-09-10 09:32:48 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World! /n and The code is updated Now.");
+        System.out.println("Hello, World! \n and The code is updated Now.");
     }
 }
